170bd3d16bebc466050072556d002b2b3ea30372
FernandoQ <octavius.sys@gmail.com>
2021-10-30 01:49:50 -0500

    culminado el video numero 7 de luis lira del curso de graphql

--- src/graphql/resolvers/character.ts
+++ src/graphql/resolvers/character.ts
@@ -1,22 +1,28 @@
 import { IResolvers } from "apollo-server-core/node_modules/graphql-tools";
+import data from "../../data/data.json";
 export const characterResolver: IResolvers = {
   Query: {
     hello: () => {
       return "Hello Wolr Tony";
     },
-    getCharacter: () => {
-      return [
-        {
-          id: 1,
-          name: "Charcter 1",
-          race: "Garrudo",
-        },
-        {
-          id: 2,
-          name: "Charcter 2",
-          race: "Hylian",
-        },
-      ];
+    getCharacters: () => {
+      return data.characters;
+    },
+    getCharacter:(root,args)=>{
+      const [found]=data.characters.filter(ch=>ch._id=args.id)
+      return found
+    }
+  }, //campo calculado con lo apredndido en midudev
+  Character: {
+    games: (root) => {
+      const gamesList: Array<any> = [];
+      root.games.map((idGame: string) => {
+        gamesList.push(...data.games.filter((game) => game._id === idGame));
+      });
+      return gamesList;
+    },
+    countGames: (root) => {
+      return root.games.length;
     },
   },
 };
